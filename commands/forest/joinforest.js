a2e07520008e4241894dd2b5e17d83798fbc95ac
YannickSanwald <82035536+YannickSanwald@users.noreply.github.com>
2021-05-20 10:00:21 +0200

    .env gitignore Test
    Hoffen das es diesmal nicht mit hochgeladen wird.

--- commands/forest/joinforest.js
+++ commands/forest/joinforest.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'joinforest',
 	description: 'Assign user to the forest group.',
 	execute(message, args) {
-		let args = message.member.guild.roles.cache.find(args => role.name === "your role");
         if (args) message.guild.members.cache.get(message.author.id).roles.add(role);
 	},
 };
