8ec30a5c6a73e06055c7251b3156cab27896d747
YannickSanwald <82035536+YannickSanwald@users.noreply.github.com>
2021-05-22 11:16:20 +0200

    Delete joinforest.js

--- commands/forest/joinforest.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'joinforest',
-	description: 'Assign user to the forest group.',
-	execute(message, args) {
-        if (args) message.guild.members.cache.get(message.author.id).roles.add(role);
-	},
-};
